a89a3f6ee885ab4b410bced91e242e29865855f8
JesaniNikhil <webnikhil26@gmail.com>
2024-05-05 16:14:43 +0530

    calculator project

---
+++ Calculator/src/App.jsx
@@ -0,0 +1,19 @@
+import styles from './App.module.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Display from './components/Display';
+import Buttons from './components/Buttons';
+function App() {
+  
+  return (
+    <>
+    <center><h1>Calculator</h1></center>
+    <div className={styles.calculator}>
+     <Display></Display>
+     <Buttons></Buttons>
+    </div>
+    </>
+  )
+}
+
+export default App
